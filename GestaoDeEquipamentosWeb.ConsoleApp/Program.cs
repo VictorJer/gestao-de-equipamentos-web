8c930d8e9dc61c79c70129de752e64f31e559bca
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-05-13 14:05:17 -0300

    add base

---
+++ GestaoDeEquipamentosWeb.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+app.UseRouting();
+app.MapDefaultControllerRoute();
+
+
+app.Run();
