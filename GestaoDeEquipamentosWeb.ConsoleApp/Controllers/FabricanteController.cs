2a152e198b882957b9c9488b659d95b8ac78bed1
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-05-20 14:29:11 -0300

    ADD record

--- GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
@@ -1,5 +1,6 @@
 using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado;
 using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado.Arquivos;
+using GestaoDeEquipamentosWeb.ConsoleApp.Models;
 using GestaoDeEquipamentosWeb.ConsoleApp.ModuloFabricante;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,7 +22,23 @@ public class FabricanteController : Controller
     {
         List<Fabricante> fabricantes = repositorioFabricante.SelecionarTodos();
 
-        return View(fabricantes);
+        List<ListarFabricantesViewModel> listarVms = new List<ListarFabricantesViewModel>();
+
+        foreach (Fabricante f in fabricantes)
+        {
+            ListarFabricantesViewModel viewModel = new ListarFabricantesViewModel(
+                f.Id,
+                f.Nome,
+                f.Email,
+                f.Telefone
+            );
+
+            listarVms.Add(viewModel);
+        }
+
+
+
+        return View(listarVms);
     }
 
 
